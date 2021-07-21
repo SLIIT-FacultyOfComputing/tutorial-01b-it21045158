008ce6e8b7ec0d345b21f9c4a3602dc48ddc4cf4
it21045158 <87773356+it21045158@users.noreply.github.com>
2021-07-21 20:12:35 +0000

    I have Written tutorial exercise 2

--- Tute02.c
+++ Tute02.c
@@ -18,7 +18,8 @@ Amount = 30 x 50 + (50-30) x 40 = 2300*/
 
 #include <stdio.h>
 
-int main() {
+int main() 
+{ 
   float distance,amount;
   printf("Enter Distance (Km) : ");
   scanf("%f",&distance);
@@ -28,4 +29,5 @@ int main() {
     amount=30*50+(distance-30)*40;
   printf("Amount = %.2f",amount);
   return 0;
+
 }
