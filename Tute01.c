4ebd55572ad11e4ec05b9947f939be2bc36627c4
it21045158 <87773356+it21045158@users.noreply.github.com>
2021-07-21 19:15:04 +0000

    I have written tutorial exercise 1

--- Tute01.c
+++ Tute01.c
@@ -5,6 +5,14 @@
 #include <stdio.h>
 
 int main() {
+  int mark1,mark2;
+  float average;
+  printf("Enter 1st subject mark : ");
+  scanf("%d",&mark1);
+  printf("Enter 2nd subject mark : ");
+  scanf("%d",&mark2);
+  average=(mark1+mark2)/2.0;
+  printf("Average Mark = %.2f",average);
   
   return 0;
 }
--- Tute02.c
+++ Tute02.c
@@ -19,6 +19,13 @@ Amount = 30 x 50 + (50-30) x 40 = 2300*/
 #include <stdio.h>
 
 int main() {
-  
+  float distance,amount;
+  printf("Enter Distance (Km) : ");
+  scanf("%f",&distance);
+  if(distance<=30)
+    amount=distance*50.0;
+  else
+    amount=30*50+(distance-30)*40;
+  printf("Amount = %.2f",amount);
   return 0;
 }
